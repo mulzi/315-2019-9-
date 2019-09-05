e0e4c1d229d817b1ba2d9f6b0e6f2d0d750d1231
mulzi <910881441@qq.com>
2019-09-05 11:51:36 +0800

    20190905开发完成

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-Math.f
+
+
+$(function() {
+    //锚点跳转滑动效果
+    $("a[href*=#],area[href*=#]").click(function() {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var $target = $(this.hash);
+        $target =
+          ($target.length && $target) || $("[name=" + this.hash.slice(1) + "]");
+        if ($target.length) {
+          var targetOffset = $target.offset().top;
+          $("html,body").animate(
+            {
+              scrollTop: targetOffset - 150
+            },
+            1000
+          );
+          return false;
+        }
+      }
+    });
+    $("#messageConLi").on("click", "li", function () {
+      //留言选项效果
+      var massagetext = document.getElementById("massagetext");
+      var mText = $(this).text();
+      massagetext.value.remove;
+      massagetext.value = mText;
+  });
+  });
+  
+
+
